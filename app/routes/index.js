43ec9372d2ce1064fda10978bb6c7aca060b5c7e
Nikola <burdebug@gmail.com>
2016-05-22 21:50:19 +0000

    tiny-url

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,139 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+module.exports = function(app, db, random) {
+
+   var collection = db.collection('urls')
+   var randomNum = random;
+
+
+   app.route('/new/http://:url')
+      .get(function(req, res) {
+
+         if (/((http?):\/\/)www.[a-z0-9\-\.]{3,}\.([a-z]{2}||[a-z]{3})$/.test('http://' + req.params.url)) {
+
+            var value;
+
+            collection.find({}, {
+               'original_url': 0,
+               'tiny_url': 0
+            }).toArray(function(err, data) {
+               value = randomNum.integer(1000, 9999);
+               for (var i = 0; i < data.length; i++) {
+
+                  if (data[i].uniqueVal == value) {
+                     i = 0;
+
+                     value = randomNum.integer(1000, 9999);
+                  }
+
+               }
+
+               collection.insert({
+                  uniqueVal: value,
+                  'original_url': 'http://' + req.params.url,
+                  'tiny_url': 'https://tiny-url-burdebug.c9users.io/' + value
+               }, function(err, data) {
+
+                  if (err)
+                     throw err
+                  res.json({
+                     'original_url': 'http://' + req.params.url,
+                     'tiny_url': 'https://tiny-url-burdebug.c9users.io/' + value
+                  })
+               })
+            })
+         }
+         else
+            res.json({
+               'message': 'invalid-url'
+            })
+      })
+
+   app.route('/new/https://:url')
+      .get(function(req, res) {
+
+         if (/((https?):\/\/)www.[a-z0-9\-\.]{3,}\.([a-z]{2}||[a-z]{3})$/.test('http://' + req.params.url)) {
+
+
+            var value;
+
+            collection.find({}, {
+               'original_url': 0,
+               'tiny_url': 0
+            }).toArray(function(err, data) {
+               value = randomNum.integer(1000, 9999);
+               for (var i = 0; i < data.length; i++) {
+
+                  if (data[i].uniqueVal == value) {
+                     i = 0;
+                     console.log('now')
+                     value = randomNum.integer(1000, 9999);
+                  }
+
+               }
+
+               collection.insert({
+                  uniqueVal: value,
+                  'original_url': 'https://' + req.params.url,
+                  'tiny_url': 'https://tiny-url-burdebug.c9users.io/' + value
+               }, function(err, data) {
+
+                  if (err)
+                     throw err
+                  res.json({
+                     'original_url': 'https://' + req.params.url,
+                     'tiny_url': 'https://tiny-url-burdebug.c9users.io/' + value
+                  })
+               })
+            })
+         }
+         else
+            res.json({
+               'message': 'invalid-url'
+            })
+
+
+      })
+
+   app.route('/list').get(function(req, res) {
+      collection.find({}).toArray(function(err, data) {
+         if (err)
+            throw err
+         res.json(data);
+      })
+
+   })
+
+   app.route('/clear').get(function(req, res) {
+      collection.remove({});
+      res.send();
+   })
+
+   app.route('/favicon.ico').get(function(req, res) {
+      res.send('favicon')
+   })
+
+   app.route('/:val').get(function(req, res) {
+
+      collection.find({
+         'uniqueVal': parseInt(req.params.val)
+      }).toArray(function(err, data) {
+
+         if (err)
+            throw err;
+
+         if (data.length)
+            res.redirect(data[0].original_url)
+         else
+            res.redirect('/')
+
+      })
+   })
 
    app.route('/')
-      .get(function (req, res) {
+      .get(function(req, res) {
          res.sendFile(process.cwd() + '/public/index.html');
       });
-
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+ 
 };
--- server.js
+++ server.js
@@ -3,24 +3,32 @@
 var express = require('express');
 var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
+var bodyParser = require('body-parser');
+var random = require('random-js')();
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+mongo.connect('mongodb://localhost:27017/tinyurlsdb', function (err, db) {
 
    if (err) {
       throw new Error('Database failed to connect!');
    } else {
       console.log('Successfully connected to MongoDB on port 27017.');
    }
+   
+   app.use(bodyParser.json());
+   
+   app.use(bodyParser.urlencoded({extended:true}));
 
    app.use('/public', express.static(process.cwd() + '/public'));
-   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+   
 
-   routes(app, db);
+   routes(app,db,random);
+   
+   
 
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
+   app.listen(8080, function () {
+      console.log('Node.js listening on port 8080...');
    });
 
 });
